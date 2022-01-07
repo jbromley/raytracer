48173a19695697032c860b7ceb1f81debec1d25a
J. Bromley <jay@appliedinvention.com>
2022-01-07 15:55:20 -0800

    Added color module with Color struct.
    Simple RGB color struct.

---
+++ src/color.rs
@@ -0,0 +1,31 @@
+use std::fmt;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Color {
+        Color { r, g, b}
+    }
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.r, self.g, self.b)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_color_fmt() {
+        let c = Color { r: 16, g: 32, b: 64 };
+        assert_eq!(format!("{}", c), "16 32 64");
+    }
+}
