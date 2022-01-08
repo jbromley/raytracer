7fbdf13e88f92fac460427fbd91da74e1f1d9547
J. Bromley <jay@appliedinvention.com>
2022-01-07 16:26:24 -0800

    Added Color::from_float.
    Create an RGB color with components ranging from 0 to 255 from floats ranging
    from 0.0 to 1.0.

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,6 @@
+use std::cmp::PartialEq;
 use std::fmt;
+
 
 #[derive(Debug, Clone, Copy)]
 pub struct Color {
@@ -11,6 +13,32 @@ impl Color {
     pub fn new(r: u8, g: u8, b: u8) -> Color {
         Color { r, g, b, }
     }
+
+    pub fn from_float(r: f64, g: f64, b: f64) -> Color {
+        if r < 0.0 || r > 1.0 {
+            panic!("Color r out of range: {}", r);
+        }
+        if g < 0.0 || g > 1.0 {
+            panic!("Color g out of range: {}", r);
+        }
+        if b < 0.0 || b > 1.0 {
+            panic!("Color b out of range: {}", r);
+        }
+
+        let max = 255.0;
+
+        Color {
+            r: (max * r) as u8,
+            g: (max * g) as u8,
+            b: (max * b) as u8,
+        }
+    }
+}
+
+impl PartialEq for Color {
+    fn eq(&self, other: &Color) -> bool {
+        self.r == other.r && self.g == other.g && self.b == other.b
+    }
 }
 
 impl fmt::Display for Color {
@@ -22,6 +50,22 @@ impl fmt::Display for Color {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_color_eq() {
+        let c1 = Color { r: 16, g: 32, b: 64, };
+        let c2 = Color { r: 16, g: 32, b: 64, };
+        assert!(c1.r == c2.r && c1.g == c2.g && c1.b == c2.b);
+    }
+
+    #[test]
+    fn test_color_from() {
+        let r = 0.0;
+        let g = 0.5;
+        let b = 1.0;
+        let c = Color::from_float(r, g, b);
+        assert_eq!(c, Color::new(0, 127, 255));
+    }
 
     #[test]
     fn test_color_fmt() {
