9b13e34d2af05d88a9a215c7a26ffc55ea1b60cb
J. Bromley <jay@appliedinvention.com>
2022-01-11 17:10:10 -0800

    Implemented world with multiple spheres.
    Normals are color-mapped for display.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use raytracer::color::Color;
-use raytracer::ray::{Hittable, Ray};
+use raytracer::ray::{Hittable, Ray, HitRecord};
 use raytracer::sphere::Sphere;
 use raytracer::vec::Vector;
 
-fn ray_color(r: Ray) -> Color {
+fn ray_color(r: Ray, world: &Vec<Sphere>) -> Color {
     let bg_color = Color::new(0.5, 0.7, 1.0);
-    let sphere = Sphere::new(Vector::new(0.0, 0.0, -1.0), 0.5);
-    match sphere.hit(&r, 0.0, f64::INFINITY) {
+
+
+    match hit_world(world, &r, 0.0, f64::INFINITY) {
         Some(hit_record) => {
-            let n = (hit_record.p - sphere.center).normalize();
-            Color::from_normal(&n)
+            Color::from_normal(&hit_record.n)
         },
         None => {
             let unit_dir = r.direction.normalize();
@@ -19,11 +19,29 @@ fn ray_color(r: Ray) -> Color {
     }
 }
 
+fn hit_world(world: &Vec<Sphere>, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+    let mut closest = t_max;
+    let mut hit_record = None;
+    for sphere in world {
+        if let Some(hit) = sphere.hit(r, t_min, closest) {
+            closest = hit.t;
+            hit_record = Some(hit);
+        }
+    }
+    hit_record
+}
+
 fn main() {
     // Image
     let aspect_ratio: f64 = 16.0 / 9.0;
     let image_width = 800;
     let image_height = ((image_width as f64) / aspect_ratio) as i32;
+
+    // World
+    let world  = vec![
+        Sphere::new(Vector::new(0.0, 0.0, -1.0), 0.5),
+        Sphere::new(Vector::new(0.0, -100.5, -1.0), 100.0),
+    ];
 
     // Camera
     let viewport_height = 2.0;
@@ -42,7 +60,7 @@ fn main() {
             let u = x as f64 / (image_width - 1) as f64;
             let v = y as f64 / (image_height - 1) as f64;
             let r = Ray::new(Vector::origin(), lower_left + u * horiz + v * vert - Vector::origin());
-            let c = ray_color(r);
+            let c = ray_color(r, &world);
             println!("{}", c);
         }
         eprintln!("{} scan lines remaining", y);
